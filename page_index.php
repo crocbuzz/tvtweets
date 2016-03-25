9839c8b7895bd0ef158b7c0cdaeaf4e8af4bff42
Alex Crocker <croc122@gmail.com>
2016-03-25 17:32:49 -0400

    Basic setup

---
+++ page_index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TV Tweets</title>
+
+        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/pure.min.css">
+        <link rel="stylesheet" href="css/grids-responsive.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/styles.css" />
+    </head>
+    <body>
+        <div id="wrapper">
+            
+        </div>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.touchSwipe.min.js"></script>
+        <script type="text/javascript" src="js/scripts.js"></script>
+    </body>
+</html>
